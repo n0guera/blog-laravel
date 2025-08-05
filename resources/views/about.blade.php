c944c821724131d369b69093509c1492f3b66101
Santiago Noguera <santiiago.noguera@gmail.com>
2025-08-05 19:53:03 -0300

    Add About and Contact views

---
+++ resources/views/about.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.app')
+
+@section('content')
+    <h2 class="text-3xl font-bold">About Us</h2>
+    <p class="mt-4 text-lg text-gray-600">This is the best blog out there.</p>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -3,3 +3,5 @@
 use Illuminate\Support\Facades\Route;
 
 Route::view('/', 'home');
+Route::view('about', 'about');
+Route::view('contact', 'contact');
