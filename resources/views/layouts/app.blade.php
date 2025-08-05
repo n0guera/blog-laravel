03ade7b5e34fd01bc78f7d57a94f27084b053d62
Santiago Noguera <santiiago.noguera@gmail.com>
2025-08-05 19:45:31 -0300

    Add main layout

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Homepage</title>
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+</head>
+
+<body class="bg-gray-100 text-gray-900">
+    <header class="bg-white shadow-md py-4">
+        <div class="container mx-auto flex justify-between items-center px-6">
+            <a href="/">
+                <h1 class="text-xl font-bold">Blog</h1>
+            </a>
+            <nav>
+                <ul class="flex space-x-6">
+                    <li><a href="/about">About us</a></li>
+                    <li><a href="/contact">Contact</a></li>
+                </ul>
+            </nav>
+        </div>
+    </header>
+    <main class="container mx-auto mt-10 px-6 text-center">
+        @yield('content')
+    </main>
+    <footer class="mt-10 py-6 bg-white text-center shadow-md">
+        <p class="text-gray-600">&copy; 2025 Blog. All rights reserved.</p>
+    </footer>
+</body>
+
+</html>
