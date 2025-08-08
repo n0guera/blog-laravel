2a248331955f64b8e437b4963be9c2492b1732ca
Santiago Noguera <santiiago.noguera@gmail.com>
2025-08-07 22:09:21 -0300

    Fix contact and about views

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.app')
 
 @section('content')
-    <h2 class="text-3xl font-bold">Contact Us</h2>
-    <p class="mt-4 text-lg text-gray-600">Email: support@website.com</p>
+    <h2 class="text-center mt-6 text-3xl font-bold">Contact Us</h2>
+    <p class="text-center mt-4 text-lg text-gray-600">Email: support@website.com</p>
 @endsection
