ecf6ce3208b395f398b57287b2cd6792e08f2248
Santiago Noguera <santiiago.noguera@gmail.com>
2026-02-09 13:16:30 -0300

    feat: add functionality to edit form

--- resources/views/categories/edit.blade.php
+++ resources/views/categories/edit.blade.php
@@ -12,8 +12,23 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900 dark:text-gray-200">
-                    <form>
-                        {{ $category->name }}
+                    <form action="{{ route('categories.update', $category) }}" method="POST">
+                        @csrf
+                        @method('PUT')
+
+                        <div>
+                            <div>
+                                <label for="name">Name:</label>
+                            </div>
+                            <input type="text" name="name" id="name" value="{{ $category->name }}"
+                                class="border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm">
+                        </div>
+                        <div>
+                            <button type="submit"
+                                class="inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 focus:bg-gray-700 active:bg-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 transition ease-in-out duration-150">
+                                Save
+                            </button>
+                        </div>
                     </form>
                 </div>
             </div>
